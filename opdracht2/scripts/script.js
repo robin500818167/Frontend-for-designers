9b2e30e504b87bee725c680ed1fc4ce988e6433a
Robin500818167 <robin.van.houten@hva.nl>
2022-04-06 15:32:25 +0200

    bingoo

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,10 @@
-// JavaScript Document
-console.log("howdy");
+
+var tbody = document.querySelector('#table tbody');
+var color = "white";
+tbody.addEventListener('click', function (e) {
+  var cell = e.target.closest('td');
+  color = cell.style.color = color === "red" ? "white" :  "red";
+});
+  
+
+
