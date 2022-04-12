13e363f5fa78293af7b1bf45046d412cf46af6b7
Robin500818167 <robin.van.houten@hva.nl>
2022-04-12 17:30:52 +0200

    spraak

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,3 +1,4 @@
+//switch
 var checkBox = document.getElementById("myCheck");
 
 function forgiveness() {
@@ -14,6 +15,63 @@ var selector = document.querySelectorAll('td input');
 }
 forgiveness();
 checkBox.addEventListener("change", forgiveness);
-  
+voiceCommandos();
 
+// Bediening
+// keyboard B
+document.addEventListener('keypress', function(event) {
+  if (event.key == "b") {
+    console.log("yeay b");
+    document.getElementById("bingo").style.display = "flex";
+  }
+});
+//Spraak
+function voiceCommandos() {
+  /* https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API */
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+  var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
+  /* de commando's */
+  var commandos = [ 'bingo'];
+  var grammar = '#JSGF V1.0; grammar commandos; public <commando> = ' + commandos.join(' | ') + ' ;'
+
+  /* het luisterobject */
+  var recognition = new SpeechRecognition();
+  var speechRecognitionList = new SpeechGrammarList();
+
+  /* als er een commando uitgesproken is */
+  function spraakAfhandelen(event) {
+    let last = event.results.length - 1;
+    let commando = event.results[0][0].transcript;
+
+    commando = commando.trim().toLowerCase();
+
+    if (commando == "bingo") {
+      console.log("yeay b");
+      document.getElementById("bingo").style.display = "flex";
+    }
+  }
+
+  function luisteren(){
+    recognition.start();
+  }
+
+  /* het luisterobject de commando's leren */
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+  recognition.continuous = false;
+  recognition.lang = 'nl';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  recognition.onresult = function(event) {
+    spraakAfhandelen(event);
+  }
+
+  recognition.onend = function() {
+    luisteren();
+  }
+
+  luisteren();
+}
